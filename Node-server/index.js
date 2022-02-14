bca0dea48257bb1d4bd52d8a5e019fa36997c206
Viraj <virajj014@gmail.com>
2022-02-15 01:49:37 +0530

    added active users indicator

--- Node-server/index.js
+++ Node-server/index.js
@@ -1,44 +1,49 @@
+let express = require("express");
+let app = express();
+const port = 8000;
 
-  let express = require("express");
-  let app = express();
-  var http = require("http").Server(app);
-  var io = require("socket.io")(http);
-  const path = require("path");
-  const mainfile = path.join(__dirname, "../");
-  app.use(express.static(mainfile));
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+const path = require("path");
+const mainfile = path.join(__dirname, "../");
+app.use(express.static(mainfile));
 
-  app.get("/", function (req, res) {
-    res.sendFile(mainfile + "/index.html");
-    // console.log(mainfile)
-  });
+app.get("/", function (req, res) {
+  res.sendFile(mainfile + "/index.html");
+  // console.log(mainfile)
+});
 
-  const users = {};
-
-  io.on("connection", (socket) => {
-    socket.on("new_user_joined", (name) => {
-      console.log("new user", name);
-      users[socket.id] = name;
-      socket.broadcast.emit("user-joined", name);
-      
-
-      socket.on("disconnect",()=>{
-      console.log("user left", name);
-      socket.broadcast.emit("user-left", name);
-    })
-    });
-
-    socket.on("send", (message) => {
-        console.log(message);
-      socket.broadcast.emit("recieve", {
-        message: message,
-        name: users[socket.id],
-      });
-    });
-
+const activeusers = {};
+let objectLength = 0;
+io.on("connection", (socket) => {
+  socket.on("new_user_joined", (username) => {
+    console.log("new user", username);
+    activeusers[socket.id] = username;
+    socket.broadcast.emit("user-joined", username);
     
+    //to get current online users
+    // let objectLength = Object.keys(activeusers).length;
+    objectLength++;
+    io.sockets.emit("user-online",objectLength);
+    // console.log(objectLength)
+
+    socket.on("disconnect", () => {
+      console.log("user left", username);
+      socket.broadcast.emit("user-left", username);
+      objectLength--;
+      io.sockets.emit("user-online",objectLength);
+    });
   });
 
-  http.listen(8000, function () {
-    console.log("Server running at port 8000");
+  socket.on("send", (message) => {
+    console.log(message);
+    socket.broadcast.emit("recieve", {
+      message: message,
+      username: activeusers[socket.id],
+    });
   });
+});
 
+http.listen(port, function () {
+  console.log(`Server running at port ${port}`);
+});
--- client.js
+++ client.js
@@ -1,45 +1,50 @@
+const socket = io();
+const form = document.getElementById("sendcont");
+const messageInput = document.getElementById("sent_msg");
+const messageContainer = document.getElementById("messagebox");
 
-  const socket = io();
-  const form = document.getElementById("sendcont");
-  const messageInput = document.getElementById("sent_msg");
-  const messageContainer = document.getElementById("messagebox");
-   
-  let audio = new Audio("/sounds/iphone_notification.mp3")
-  
-  const append = (message, position) => {
-    const messageElement = document.createElement("div");
-    messageElement.innerText = message;
-    messageElement.classList.add("message");
-    messageElement.classList.add(position);
-    messageContainer.appendChild(messageElement);
+let audio = new Audio("/sounds/iphone_notification.mp3");
 
-    if(position == 'left' || position == 'center'){
-      audio.play();
-    }
-  };
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.appendChild(messageElement);
 
-  form.addEventListener('submit',(evt)=>{
-      evt.preventDefault();
-      const message = messageInput.value;
-      
-      append(`You : ${message}`,"right");
-      socket.emit('send',message)
-      messageInput.value = "";
-  })
+  if (position == "left" || position == "center") {
+    audio.play();
+  }
+};
 
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const message = messageInput.value;
 
+  append(`You : ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
 
-  const name = prompt("Enter your name");
-  socket.emit("new_user_joined", name);
+let username = prompt("Enter your username");
+while (username == null || username == "") {
+  alert ("Username can't be empty");
+  username = prompt("Enter your username");
+}
+socket.emit("new_user_joined", username);
 
-  socket.on("user-joined", (name) => {
-    append(`${name} joined the party`, "center");
-  });
+socket.on("user-joined", (username) => {
+  append(`${username} joined the party`, "center");
+});
 
-  socket.on('recieve', (data) => {
-    append(`${data.name} : ${data.message}`, "left");
-  });
+socket.on("recieve", (data) => {
+  append(`${data.username} : ${data.message}`, "left");
+});
 
-  socket.on("user-left", (name) => {
-    append(`${name} left the party`, "left");
-  });
+socket.on("user-online", (objectlength) => {
+  document.getElementById("activeusers").innerText = `${objectlength} Active`;
+});
+
+socket.on("user-left", (username) => {
+  append(`${username} left the party`, "center");
+});
